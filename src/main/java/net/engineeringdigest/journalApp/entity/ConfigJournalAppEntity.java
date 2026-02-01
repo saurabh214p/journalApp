f34705686df94d0a91cf9d7989286a68e83dbb4d
saurabh214p <sp666532@gmail.com>
2026-02-01 17:38:56 +0530

    weather api

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/ConfigJournalAppEntity.java
@@ -0,0 +1,21 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor
+public class ConfigJournalAppEntity {
+
+
+    private String key;
+    private String value;
+
+
+}
